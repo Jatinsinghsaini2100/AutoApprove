5cf9836a7ece89a87dbaf2ecaa3345743153fdb6
Jatinsinghsaini2100 <128669990+Jatinsinghsaini2100@users.noreply.github.com>
2024-01-25 22:46:16 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,9 +4,9 @@ class Config:
     API_ID = int(getenv("API_ID", ""))
     API_HASH = getenv("API_HASH", "")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
-    FSUB = getenv("FSUB", "Venom_Chatz")
+    FSUB = getenv("FSUB", "Sastatony")
     CHID = int(getenv("CHID", ""))
-    SUDO = list(map(int, getenv("SUDO", "6306738739").split()))
+    SUDO = list(map(int, getenv("SUDO", "5366891026").split()))
     MONGO_URI = getenv("MONGO_URI", "")
     
 cfg = Config()
